b4cb43b82c73fa71523ba14ee08e920d983d3a54
MaddoxGraham <57898176+MaddoxGraham@users.noreply.github.com>
2023-06-21 23:34:05 +0200

    formphoto_update

--- src/app/new-produit/new-produit.component.ts
+++ src/app/new-produit/new-produit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProduitService } from '../services/produits.services';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Observable, map } from 'rxjs';
+import { Observable, distinctUntilChanged, map } from 'rxjs';
 import { Produits } from '../models/produits.model';
 import { ActivatedRoute } from '@angular/router';
 import { CategorieService } from '../services/categories.services';
@@ -16,6 +16,8 @@ export class NewProduitComponent implements OnInit{
   createForm!:FormGroup;
   produitPreview$!: Observable<Produits>;
   currentCategoryId!: number | null;
+  photosTab: string[] = [];
+ 
 
   constructor(private produitService: ProduitService,
     private route : ActivatedRoute,
@@ -27,18 +29,25 @@ export class NewProduitComponent implements OnInit{
         ShortLibel: [null],
          LongLibel: [null],
          prxHt: [null],
-         photos:[null]
+         photos0:[null],
+         photos1:[null],
+         photos2:[null],
+         photos3:[null],
+         photos4:[null],
+         photos5:[null],
       });
 
     // Récupérez l'ID de la catégorie depuis le service
     this.currentCategoryId = this.categoryService.getCurrentCategoryId();
-    
+
 
     this.produitPreview$ = this.createForm.valueChanges.pipe(
+     
         map(formvalue => ({ 
           ...formvalue,
           slug: slugify(formvalue.ShortLibel),
           categorie: this.currentCategoryId,
+          photos:this.photosTab
         }))
       )
     
@@ -46,9 +55,43 @@ export class NewProduitComponent implements OnInit{
   onSubmitForm(): void {
     console.log(this.createForm.value);
 }
+
+// deleteField(): void {
+//   const inputValue = this.createForm.value.photos;
+//   const index = this.photosTab.findIndex(photo => photo === inputValue);
+
+//   if (index !== -1) {
+//     this.photosTab.splice(index, 1);
+//   }
+// }
+
+addField(event: Event): void {
+  const currentButton = event.target as HTMLButtonElement;
+  const currentId = currentButton.id;
+  const index = parseInt(currentId.slice(8));
+  const srcActuel = this.createForm.value[`photos${index}`];
+if (srcActuel) {
+    this.photosTab.push(srcActuel);
+  
+  console.log(this.photosTab);
+}
+
+
+  if (index > 5) {
+    return; // Limite maximale atteinte, ne pas ajouter de nouvel champ
+  }
+
+  const newId = `photos${index + 1}`;
+  const nextElement = document.getElementById(newId);
+  if (nextElement) {
+    nextElement.classList.toggle('toggle');
+  }
+}
+
 }
 
 function slugify(text: string): string {
+
   const normalizedText = text.replace(/[^a-zA-Z0-9\s]/g, "").toLowerCase();
   const slug = normalizedText.replace(/\s+/g, "-");
 
